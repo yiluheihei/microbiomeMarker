d27b1b9778b1d9bfd2e4a71d5d18890d551a6aed
yiluheihei <yiluheihei@gmail.com>
2019-10-21 17:20:32 +0800

    use phyloseq to create caporaso data

--- data-raw/Caporaso.R
+++ data-raw/Caporaso.R
@@ -1,7 +1,7 @@
 ## Human Moving Picture from MicrobiomeAnalyst server
 
 library(MicrobiomeAnalystR)
-library(magrittr)
+library(phyloseq)
 
 download.file("https://www.microbiomeanalyst.ca/MicrobiomeAnalyst/resources/data/treebiom.zip",
   "data-raw/caporaso.zip"
@@ -9,18 +9,13 @@ download.file("https://www.microbiomeanalyst.ca/MicrobiomeAnalyst/resources/data
 unzip("data-raw/caporaso.zip", exdir = "data-raw/")
 file.rename("data-raw/treebiom/", "data-raw/caporaso/")
 
-mbset <- Init.mbSetObj() %>%
-  SetModuleType("mdp") %>%
-  Read16SAbundData("data-raw/caporaso/otu_table_mc2_w_tax_no_pynast_failures.biom","biom","GreengenesID","F") %>%
-  ReadSampleTable("data-raw/caporaso/map.txt") %>%
-  ReadTreeFile("data-raw/caporaso/rep_set.tre") %>%
-  SanityCheckData("biom") %>%
-  PlotLibSizeView("norm_libsizes_0","png") %>%
-  CreatePhyloseqObj("biom","GreengenesID","F") %>%
-  ApplyAbundanceFilter("prevalence", 4, 0.2) %>%
-  ApplyVarianceFilter("iqr", 0.1) %>%
-  PerformNormalization("none", "colsum", "none")
-
-caporaso_phyloseq <- mbset$dataSet$norm.phyobj
+ps <- import_biom(
+  "data-raw/caporaso/otu_table_mc2_w_tax_no_pynast_failures.biom",
+  treefilename = "data-raw/caporaso/rep_set.tre",
+  parseFunction = parse_taxonomy_greengenes
+)
+sampledata <- read.delim("data-raw/caporaso/map.txt", row.names = 1) %>%
+  sample_data()
+caporaso_phyloseq <- merge_phyloseq(ps, sampledata)
 
 usethis::use_data(caporaso_phyloseq)
