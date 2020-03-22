a681705471649b24cc65609b71aa202bf827bff9
yiluheihei <yiluheihei@gmail.com>
2020-03-22 15:54:41 +0800

    feat: wilcoxon test for subclass

--- R/lefse.R
+++ R/lefse.R
@@ -4,37 +4,74 @@
 #'
 #' @param ps a \code{\link[phyloseq]{phyloseq-class}} object
 #' @param class character, the column name to specify the class
-#' @param p_cutoff numeric, p value cutoff, default 0.05
+#' @param kw_cutoff numeric, p value cutoff of kw test, default 0.05
+#' @param wilcoxon_cutoff numeric, p value cutoff of wilcoxon test, default 0.05
 #' @param norm norm set the normalization value
-#' @param by_otu by otu or summarized taxa, default false
+#' @param summarized whether by summarized taxa or feature, default TRUE
 #' @param lda_cutoff numeric, lda score cutoff, default 2
-#' @param correct_p use corrected p value (fdr) or not, default false
+#' @param bootstrap_n integer, the number of bootstrap iteration for LDA,
+#'   default 30
+#' @param bootstrap_fraction numberic, the subsampling fraction value for each
+#'   bootstrap iteration, default `2/3`
+#' @param multicls_strat logical, for multiple class tasks, whether the test is
+#'   performed in a one-against one (more strict) or in a one-against all
+#'   setting, default `FALSE`.
+#' @param correct multiple testing options, 0 for no correction (default), 1 for
+#'   independent comparisons, 2 for independent comparison
+#' @param sample_min integer, minimum number of samples per subclass for
+#'   performing wilcoxon test, default 10
+#' @param only_same_subcls logical, whether perform the wilcoxon test only
+#'   among the subclasses with the same name, default `FALSE`
+#' @param curv logical, whether perform the wilcoxon test using the
+#'   Curtis's approach, defalt `FALSE`
 #'
 #' @importFrom  dplyr mutate filter arrange rowwise select
 #' @importFrom  purrr map_dbl pmap_dbl pmap_chr
 #' @importFrom stats p.adjust
 #' @export
+#' @return a data frame contains five variables:
+#' * `feature`, significantly different features.
+#' * `enrich_group`, the class of the differential features enriched
+#' * `log_max_mean`, the logarithm value of the highest mean among all the
+#'   classes
+#' * `lda`, logarithmic LDA score
+#' * `p_value`, p value of kw test
 #' @author Yang Cao \email{yiluheihei@gmail.com}
 #' @references Segata, Nicola, et al. Metagenomic biomarker discovery and
 #' explanation. Genome biology 12.6 (2011): R60.
 lefse <- function(ps,
                   class,
                   norm = 1000000,
-                  by_otu = FALSE,
-                  p_cutoff = 0.05,
+                  summarized = TRUE,
+                  kw_cutoff = 0.05,
                   lda_cutoff = 2,
-                  correct_p = FALSE) {
+                  bootstrap_n = 30,
+                  bootstrap_fraction = 2/3,
+                  wilcoxon_cutoff = 0.05,
+                  multicls_strat = FALSE,
+                  correct = c("0", "1", "2"),
+                  sample_min = 10,
+                  only_same_subcls = FALSE,
+                  curv = FALSE) {
   if (!inherits(ps, "phyloseq")) {
     stop("`ps` must be phyloseq object")
   }
+
+  correct <- match.arg(correct)
+  correct <- as.numeric(correct)
 
   # filter the taxa whose abundance is zero
   ps <- phyloseq_qc(ps)
 
   sample_meta <- sample_data(ps)
-  class <- sample_meta[[class]]
+  cls_info <- lefse_format_input(sample_meta, class)
+  cls <- cls_info$cls
+  subcls <- cls_info$subcls
+  cls_hie <- cls_info$cls_hie
 
-  if (by_otu) {
+  # not supported subclass now
+
+  if (!summarized) {
     otus <- otu_table(ps)
     if (taxa_are_rows(otus)) {
       otus <- t(otus)
@@ -51,77 +88,50 @@ lefse <- function(ps,
   }
 
   # kw rank sum test among classes
-  kw_p <- map_dbl(otus, ~ kruskal.test(.x, class)$p.value)
-  kw_fdr <- p.adjust(kw_p, method = "fdr")
+  kw_p <- purrr::map_dbl(otus, ~ kruskal.test(.x, cls)$p.value)
+  sig_ind <- kw_p <= kw_cutoff
+  sig_otus <- otus[, sig_ind]
 
-  # wilcoxon rank sum test is not preformed if there is no subclass
+  # wilcoxon rank sum test is preformed for each class, if there is no subclass
+  features_nms <- names(sig_otus)
+  wilcoxon_p <- purrr::map2_lgl(
+    sig_otus, features_nms,
+    ~ test_rep_wilcoxon(
+      subcls, cls_hie,
+      .x, .y,
+      wilcoxon_cutoff = wilcoxon_cutoff,
+      multicls_strat = multicls_strat,
+      correct = correct,
+      sample_min = sample_min,
+      only_same_subcls = only_same_subcls,
+      curv = curv
+    )
+  )
+  sig_otus <- sig_otus[, wilcoxon_p]
 
-  if (correct_p) {
-    sig_ind <- kw_fdr <= p_cutoff
-  } else {
-    sig_ind <- kw_p <= p_cutoff
-  }
-  otus <- otus[, sig_ind]
+  # mean abundance in each group
+  otus_enriched_group <- get_feature_enrich_group(cls, sig_otus)
 
-  otus_enriched_group <- get_feature_enrich_group(class, otus)
+  # bootsrap iteration of lda
   ldas <- bootstap_lda(
-    otus,
-    boot_n = 30,
-    class = class,
-    sample_fract = 0.67
+    sig_otus,
+    boot_n = bootstrap_n,
+    class = cls,
+    sample_fract = bootstrap_fraction
   )
 
   res <- data.frame(
-    feature = names(otus),
-    group = otus_enriched_group$group,
+    feature = names(sig_otus),
+    enrich_group = otus_enriched_group$group,
     log_max_mean = otus_enriched_group$log_max_mean,
     lda = ldas,
     p_value = kw_p[sig_ind],
-    fdr = kw_fdr[sig_ind],
     stringsAsFactors = FALSE
   )
   res <- filter(res, .data$lda >= lda_cutoff)
 
   res
-
-
-  # lda analysis
-  # lda_res <- MASS::lda(
-  #   class ~ .,
-  #   data = otus,
-  #   tol = 1.0e-10
-  # )
-
-  # dplyr verbs drop the row names automatically, otu_id is saved and added after
-  # dplyr manipulation complete
-  # otu_id <- colnames(lda_res$means)
-  # lda_mean <- t(lda_res$means) %>%
-  #   tibble::as_tibble()
-  #
-  # lda_max <- pmap_dbl(lda_mean, max)
-  # lda_min <- pmap_dbl(lda_mean, min)
-  # enriched <- pmap_chr(lda_mean, enrich_group)
-  #
-  # lda_mean <- mutate(lda_mean, max = lda_max, min = lda_min) %>%
-  #   mutate(lda_score = signif(log10(1 + abs(max - min)/2), digits = 5)) %>%
-  #   mutate(p_value = kw_p[sig_ind], fdr = kw_fdr[sig_ind], enrich_group = enriched) %>%
-  #   mutate(otu = gsub("`", "", otu_id))# by default, as_tibble add ` around invalid names
-  #
-  # # significant feature,  order result by lda_score
-  # sig_feature <- filter(lda_mean, lda_score >= lda_cutoff) %>%
-  #   arrange(desc(lda_score))
-
-  # sig_feature
 }
-
-#' helper function to extract enrich group
-#' @noRd
-# enrich_group <- function(...) {
-#   lda_mean <- c(...)
-#   group_name <- names(lda_mean)
-#
-#   group_name[which.max(lda_mean)]
-# }
 
 # suppress the checking notes “no visible binding for global variable", which is
 # caused by NSE
---
+++ R/lefse_utilities.R
@@ -0,0 +1,430 @@
+# enrich group of the feature ---------------------------------------------
+
+#' get the means abundance of each class for each feature
+#' @noRd
+get_feature_enrich_group <- function(class, feature) {
+  feature$class <- class
+  feature_mean <- group_by(feature, class) %>%
+    summarise(colMeans(cur_data(), ) %>% bind_rows())
+  feature_enrich_index <- select(feature_mean, -class) %>%
+    purrr::map_dbl(which.max)
+
+  feature_enrich_group <- feature_mean$class[feature_enrich_index]
+  names(feature_enrich_group) <- names(feature)[names(feature) != "class"]
+  feature_max_mean <- purrr::map2_dbl(
+    select(feature_mean, -class),
+    feature_enrich_index,
+    ~ .x[.y]
+  )
+  # log10(max(feature_max_mean), 1)
+  feature_max_mean[feature_max_mean < 1] <- 1
+
+  return(list(
+    group = feature_enrich_group,
+    log_max_mean = log10(feature_max_mean)
+  ))
+}
+
+
+# bootstrap iteration LDA -------------------------------------------------
+
+#' check bootstrap sample, not contast within class and too fewer in each
+#' class
+#'
+#' @param feature_abudance data.frame, significant feature abundance,
+#'   where columns represents samples and rows represents features
+#' @param sample_indx numeric vector, sample index be bootstraped
+#' @param sample_min integer, min samples in each class
+#' @param class character vector, class of all samples
+#'
+#' @noRd
+check_bootstrap_sample <- function(feature_abudance,
+                                   sample_indx,
+                                   sample_min,
+                                   class) {
+  feature_abudance <- feature_abudance[, sample_indx]
+  class_n <- length(unique(class))
+  class <- class[sample_indx]
+
+  if (length(unique(class)) < class_n) {
+    return(FALSE)
+  }
+
+  for (cls in unique(class)) {
+    if (sum(class == cls) < sample_min) {
+      return(FALSE)
+    }
+
+    # sig feature smaller than min sample count
+    cls_abundance <- feature_abudance[, class == cls]
+
+    for (i in seq_along(nrow(cls_abundance))) {
+      unique_abd <- length(unique(cls_abundance[i, ]))
+
+      if ((unique_abd <= sample_min && sample_min > 1) || (unique_abd <= 1 && sample_min == 1)) {
+        return(FALSE)
+      }
+    }
+  }
+
+  return(TRUE)
+}
+
+#' bootstrap iteration of samples for lda analysis
+#' @importFrom dplyr group_by summarize cur_data
+#' @noRd
+bootstap_lda <- function(feature_abundance,
+                         boot_n,
+                         class,
+                         sample_fract) {
+  ldas <- purrr::rerun(
+    boot_n,
+    bootstap_lda_one(
+      feature_abundance,
+      class,
+      sample_fract
+    )
+  ) %>%
+    purrr::transpose() %>%
+    purrr::map(bind_rows) %>%
+    bind_rows()
+
+  mean_lds <- colMeans(ldas)
+  mean_lds <- sign(mean_lds) * log10(1 + abs(mean_lds))
+
+  mean_lds
+}
+
+bootstap_lda_one <- function(feature_abundance,
+                             class,
+                             sample_fract) {
+  sample_groups <- unique(class)
+  class_count <- table(class)
+  feature_abundance$class <- class
+
+  feature_abundance <- preprocess_feature_all(feature_abundance, class)
+
+  sample_n <- nrow(feature_abundance)
+  random_n <- floor(sample_n * sample_fract)
+  class_n <- length(sample_groups)
+  sample_min <- floor(min(class_count) * sample_fract * sample_fract * 0.5) %>%
+    max(1)
+
+  # class vs class
+  pairs <- utils::combn(sample_groups, 2, simplify = FALSE) %>%
+    purrr::map(sort, decreasing = TRUE)
+
+  while (TRUE) {
+    # random select samples using bootstrap method
+    sample_indx <- sample(sample_n, random_n, replace = TRUE)
+
+    is_checked <- check_bootstrap_sample(
+      feature_abundance,
+      sample_indx,
+      sample_min,
+      class
+    )
+    if (is_checked) {
+      break
+    }
+  }
+
+  lda <- purrr::map(
+    pairs,
+    ~ cal_pair_lda(feature_abundance, sample_indx, .x)
+  )
+  names(lda) <- purrr::map(pairs, paste, collapse = " -VS- ")
+
+  lda
+}
+
+#' calculate lda score of single pair groups
+#' @noRd
+cal_pair_lda <- function(feature_abundance,
+                         sample_indx,
+                         pair) {
+  sample_feature_abundance <- feature_abundance[sample_indx, ]
+
+  # reference lefse.py in lefse
+  lda_res <- suppressWarnings(
+    MASS::lda(
+      class ~ .,
+      data = sample_feature_abundance,
+      tol = 1.0e-10
+    )
+  )
+  w <- lda_res$scaling[, 1]
+  w_unit <- w/sqrt(sum(w^2))
+  feature_remove_class <- sample_feature_abundance[-1]
+
+  # not support subclass and subject argument in lefse
+
+  ld <- as.matrix(feature_remove_class)%*%w_unit
+  group1_indx <- sample_feature_abundance$class == pair[1]
+  effect_size <- abs(mean(ld[group1_indx]) - mean(ld[-group1_indx]))
+  wfinal <- w_unit * effect_size
+  lda_means <- lda_res$means
+  lda_row_nms <- row.names(lda_means)
+  feature_n <- ncol(lda_means)
+  coeff <- ifelse(is.nan(wfinal), 0, abs(wfinal))
+
+  res <- purrr::map(
+    pair,
+    function(x) {
+      if (x %in% lda_row_nms) {
+        lda_means[x, ]
+      } else {
+        rep(0, feature_n)
+      }
+    }
+  )
+  names(res) <- pair
+
+  feature <- names(feature_remove_class)
+  lda_score <- purrr::map_dbl(
+    seq_along(feature),
+    function(i) {
+      gm <- abs(res[[1]][i] - res[[2]][i])
+      return(gm + coeff[i] * 0.5)
+    }
+  )
+  names(lda_score) <- feature
+
+  lda_score
+
+}
+
+
+#' feature abundance preprocess
+#' @noRd
+preprocess_feature_all <- function(x, class) {
+  res <- group_by(
+    x,
+    class
+  ) %>%
+    summarise(purrr::map_df(cur_data(), preprocess_feature))
+
+  res
+}
+
+preprocess_feature <- function(x) {
+  if (length(unique(x)) <= max(length(x)*0.5, 4)) {
+    x <- purrr::map_dbl(x, ~ abs(.x + rnorm(1, 0, max(.x*0.05, 0.01))))
+  }
+
+  x
+}
+
+# wilcoxon test within the same class-------------------------------------------
+
+#' wilcoxon test in each subclass
+#'
+#' @param subcls character vector, length equal to the number of samples
+#' @param cls_hie list, length equal to the number of classes, class hierarchy
+#' @param feats_abd numeric vector, abundance profile of a given feature
+#' @param feats_name character, feature names
+#' @param wilcoxon_cutoff the cutoff for the wilcoxon test, default 0.05
+#' @param multicls_strat logical, for multiple class tasks, whether the test is
+#'   performed in a one-against one (more strict) or in a one-against all
+#'   setting, default `FALSE`.
+#' @param correct multiple testing options, 0 for no correction (default), 1 for
+#'   independent comparisons, 2 for independent comparison
+#' @param sample_min integer, minimum number of samples per subclass for
+#'   performing wilcoxon test, default 10
+#' @param only_same_subcls logical, whether perform the wilcoxon test only
+#'   among the subclasses with the same name, default `FALSE`
+#' @param curv logical, whether perform the wilcoxon test using the
+#'   Curtis's approach, defalt `FALSE`
+#'
+#' @noRd
+feats_abd <- readr::read_csv("~/Desktop/feats.csv", col_names = FALSE) %>%
+  unlist()
+feats_name <- "k__Bacteria.p__Firmicutes.c__Clostridia.o__Clostridiales.f__Clostridiaceae.g__Clostridium.NA"
+subcls <- c(rep("CD_subcl", 23), rep("Control_subcl", 20))
+cls_hie <- list(Control = "Control_subcl", CD = "CD_subcl")
+test_rep_wilcoxon <- function(subcls,
+                              cls_hie,
+                              feats_abd,
+                              feats_name,
+                              wilcoxon_cutoff = 0.05,
+                              multicls_strat = FALSE,
+                              correct = 0,
+                              sample_min = 10,
+                              only_same_subcls = FALSE,
+                              curv = FALSE) {
+  if (!correct %in% c(0, 1, 2)) {
+    stop("`correct` must be 0, 1 or 2")
+  }
+
+  cls_nms <- names(cls_hie)
+  pairs <- utils::combn(cls_nms, 2, simplify = FALSE)
+  tot_ok <- 0
+
+  all_diff <- list()
+
+  for (pair in pairs) {
+
+    dir_cmp <- "not_set"
+
+    subcls1 <- cls_hie[[pair[1]]]
+    subcls1_n <- length(subcls1)
+    subcls2 <- cls_hie[[pair[2]]]
+    subcls2_n <- length(subcls2)
+
+    # multiple correction
+    if (correct != 0) {
+      wilcoxon_cutoff <- ifelse(
+        correct == 2,
+        wilcoxon_cutoff * subcls1_n * subcls2_n,
+        1 - (1 - wilcoxon_cutoff)^(subcls1_n * subcls2_n)
+      )
+    }
+
+    ok <- 0
+    curv_sign <- 0
+    first <- TRUE
+
+    for (i in seq_along(subcls1)) {
+      br <- FALSE
+      for (j in seq_along(subcls2)) {
+        if (only_same_subcls &&
+            gsub(pair[1], "", subcls1[i]) != gsub(pair[2], "", subcls2[j])
+        ) {
+          ok <- ok + 1
+          next
+        }
+
+        cls1_abd <- feats_abd[subcls == subcls1[i]]
+        cls2_abd <- feats_abd[subcls == subcls2[j]]
+        med_comp <- FALSE
+
+        if (length(cls1_abd) < sample_min || length(cls2_abd) < sample_min) {
+          med_comp <- TRUE
+        }
+
+        sx <- stats::median(cls1_abd)
+        sy <- stats::median(cls2_abd)
+
+        if (cls1_abd[1] == cls2_abd[1] &&
+            length(unique(cls1_abd)) == 1 &&
+            length(unique(cls2_abd)) == 1
+        ) {
+          tres <- FALSE
+          first <- FALSE
+        } else if (!med_comp) {
+          x <- c(cls1_abd, cls2_abd)
+          y <- factor(
+            c(rep(1, length(cls1_abd)),
+              rep(2, length(cls2_abd))
+            )
+          )
+          pv <- coin::wilcox_test(
+            x ~ y,
+            data = data.frame(x, y)
+          )
+          pv <- coin::pvalue(pv)
+          tres <- pv < wilcoxon_cutoff * 2
+        }
+
+        if (first) {
+          first <-  FALSE
+
+          if (!curv && (med_comp || tres)) {
+            dir_cmp <- sx < sy
+          } else if (curv) {
+            dir_cmp <- NULL
+            if (med_comp || tres) {
+              curv_sign <- curv_sign + 1
+              dir_cmp <- sx < sy
+            }
+          } else {
+            br <- TRUE
+          }
+        } else if (!curv && med_comp) {
+          if ((sx < sy) != dir_cmp || sx == sy) {
+            br <- TRUE
+          }
+        } else if (curv) {
+          if (tres && is.null(dir_cmp)) {
+            curv_sign <- curv_sign + 1
+            dir_cmp <- sx < sy
+          }
+
+          if (tres && dir_cmp != (sx < sy)) {
+            br <- TRUE
+            curv_sign <- curv_sign - 1
+          }
+        } else if (!tres || (sx < sy) != dir_cmp || sx == sy) {
+          br <- TRUE
+        }
+
+        if (br) {
+          break
+        }
+        ok <- ok + 1
+      }
+      if (br) {
+        break
+      }
+    }
+
+    diff <- ifelse(
+      curv,
+      curv_sign > 0,
+      ok == subcls1_n * subcls2_n
+    )
+
+    if (diff) tot_ok <- tot_ok + 1
+    if (!diff && multicls_strat) {
+      return(FALSE)
+    }
+    if (diff && !multicls_strat) all_diff <- c(all_diff, pair)
+  }
+
+  if (!multicls_strat) {
+    tot_k <- length(cls_hie)
+
+    for (k in names(cls_hie)) {
+      nk <- 0
+      for (a in all_diff) {
+        if (k %in% a) nk <- nk + 1
+      }
+
+      if (nk == tot_k - 1) {
+        return(TRUE)
+      }
+    }
+
+    return(FALSE)
+  }
+
+  return(TRUE)
+}
+
+# format input ------------------------------------------------------------
+
+#' format lefse input
+#'
+#' @param sample_meta a data.frame like object, sample metadata
+#' @param cls character variable of class
+#' @param subcls character variable of subclass, default `NULL`, no subclass
+#'
+#' @noRd
+#'
+#' @return  a list, contains class, subclass, and class hierarchy
+lefse_format_input <- function(sample_meta, cls, subcls = NULL) {
+  cls <- sample_meta[[cls]]
+  cls_nms <- unique(cls)
+
+  if (is.null(subcls)) {
+    subcls <- paste0(cls, "_subcls")
+    subcls_nms <- paste0(cls_nms, "_subcls")
+  } else {
+    stop("Not support subcls right now")
+  }
+
+  cls_hie <- as.list(subcls_nms)
+  names(cls_hie) <- cls_nms
+
+  return(list(cls = cls, subcls = subcls, cls_hie = cls_hie))
+}
