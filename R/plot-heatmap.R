61288e1638d7308d6e00eed577f7cb39d69ba07b
bioBDAI-hui123 <h303989731@gmail.com>
2021-11-29 16:58:07 +0100

    improve heatmap visual

--- R/plot-heatmap.R
+++ R/plot-heatmap.R
@@ -14,10 +14,15 @@
 #'  clustering in markers (rows) and samples (cols), default `FALSE`.
 #' @param sample_label logical, controls whether to show the sample labels in
 #'   the heatmap, default `FALSE`.
+#' @param scale_by_row logical, controls whether to scale the heatmap by the
+#'  row (marker) values, default `FALSE`.
+#' @param use_gghue logical, controls whether to use the default ggplot2
+#'  palette, default `TRUE`.
 #' @param ... extra arguments passed to [`ComplexHeatmap::Heatmap()`].
 #' @export
 #' @seealso [`transform_abundances`],[`ComplexHeatmap::Heatmap()`]
 #' @importFrom ComplexHeatmap Heatmap HeatmapAnnotation
+#' @importFrom grDevices hcl
 #' @return a [`ComplexHeatmap::Heatmap-class`] object.
 #' @examples
 #' data(kostic_crc)
@@ -42,6 +47,8 @@ plot_heatmap <- function(mm,
     label_level = 1,
     max_label_len = 60,
     sample_label = FALSE,
+    scale_by_row = FALSE,
+    use_gghue = TRUE,
     group,
     ...) {
     stopifnot(inherits(mm, c("microbiomeMarker", "marker_table")))
@@ -78,6 +85,10 @@ plot_heatmap <- function(mm,
     }
 
     abd <- as(otu_table(mm), "matrix")
+    if (scale_by_row) {
+        abd <- scale_rows(abd)
+    }
+
     marker_abd <- abd[match(marker$feature, row.names(abd)), ] %>%
         as.data.frame()
 
@@ -105,15 +116,43 @@ plot_heatmap <- function(mm,
         lgd_title <- "Log10 Abundance"
     }
 
+    if (scale_by_row) {
+        lgd_title <- "Row Z-score"
+    }
+
+    if (use_gghue) {
+        hues <- gg_hue(length(group_lvl))
+        names(hues) <- group_lvl
+        col_hues <- list(Group = hues)
+   } else {
+       col_hues <- NULL
+   }
 
     p <- Heatmap(
         as.matrix(marker_abd),
         cluster_rows = cluster_marker,
         cluster_columns = cluster_sample,
-        top_annotation = HeatmapAnnotation(Group = column_nms),
+        top_annotation = HeatmapAnnotation(Group = column_nms, col = col_hues),
         name = lgd_title,
         ...
     )
 
     p
+}
+
+#' Scale the heatmap by the row (marker) values
+#' @keywords internal
+#' @noRd
+scale_rows <- function(x) {
+    m <- apply(x, 1, mean, na.rm = T)
+    s <- apply(x, 1, sd, na.rm = T)
+    return((x - m) / s)
+    }
+
+#' Generate ggplot2 palette
+#' @keywords internal
+#' @noRd
+gg_hue <- function(n) {
+  hues <- seq(15, 375, length = n + 1)
+  hcl(h <- hues, l = 65, c = 100)[1:n]
 }
