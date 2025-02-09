22c4b79817f4bc8beedd474f5ee98592dc3b037c
caoyang <yiluheihei@gmail.com>
2025-02-09 21:57:58 +0800

    redocumnet, delete bioc deprecated info

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-    msg <- sprintf(
-        "Package '%s' is deprecated and will be removed from Bioconductor
-         version %s", pkgname, "3.21")
-    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
-}
